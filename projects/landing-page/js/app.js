d35f58bb38554028ea1fb94d049b1d3458e2141e
Riccardo Foti <riccardofoti97@gmail.com>
2021-02-15 16:18:48 +0000

    Started creating navbar

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,6 +17,15 @@
  * Define Global Variables
  * 
 */
+const navbarList = document.getElementById("navbar__list");
+const section1 = document.getElementById("section1");
+const section1Title = section1.getElementsByTagName('h2')[0];
+
+const section2 = document.getElementById("section2");
+const section2Title = section2.getElementsByTagName('h2')[0];
+
+const section3 = document.getElementById("section3");
+const section3Title = section3.getElementsByTagName('h2')[0];
 
 
 /**
@@ -24,7 +33,12 @@
  * Start Helper Functions
  * 
 */
-
+function createAndAddNavbarElement(sectionTitle){       // Creates an element and adds it to the navbar, by taking sectionTitle as an attribute
+    const element = document.createElement("li");
+    const elementContent = document.createTextNode(sectionTitle.innerText);
+    element.appendChild(elementContent);
+    navbarList.appendChild(element);
+}
 
 
 /**
@@ -35,6 +49,11 @@
 
 // build the nav
 
+createAndAddNavbarElement(section1Title);
+createAndAddNavbarElement(section2Title);
+createAndAddNavbarElement(section3Title);
+navbarList.style.color = "black";
+navbarList.style.float = "left";
 
 // Add class 'active' to section when near top of viewport
 
