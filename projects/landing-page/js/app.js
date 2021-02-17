f0741e1e9cf5241bc16234dd10ba0679f45ce356
Riccardo Foti <riccardofoti97@gmail.com>
2021-02-17 16:40:33 +0000

    Defined setAsActive function

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -21,13 +21,10 @@ const navbarList = document.getElementById("navbar__list");
 const sections = document.querySelectorAll("section");
 
 const section1 = document.getElementById("section1");
-// const section1Title = section1.getElementsByTagName('h2')[0];
 
 const section2 = document.getElementById("section2");
-// const section2Title = section2.getElementsByTagName('h2')[0];
 
 const section3 = document.getElementById("section3");
-// const section3Title = section3.getElementsByTagName('h2')[0];
 
 
 /**
@@ -35,14 +32,6 @@ const section3 = document.getElementById("section3");
  * Start Helper Functions
  * 
 */
-// function createAndAddNavbarElement(section){       // Creates an element and adds it to the navbar, by taking section as a parameter
-//     element = document.createElement("li");
-//     const elementContent = document.createTextNode(section.getAttribute("data-nav"));       // Adds the data-nav of the section as the text content of li
-//     element.classList.add("menu__link");
-//     element.appendChild(elementContent);
-//     navbarList.appendChild(element);
-//     return element;
-// }
 
 function createNavbar(){
     for(let item of sections){
@@ -52,8 +41,8 @@ function createNavbar(){
         navbarList.appendChild(element)
     };
 };
-createNavbar();
 
+createNavbar();
 
 
 /**
@@ -64,9 +53,6 @@ createNavbar();
 
 // build the nav
 
-// const element1 = createAndAddNavbarElement(section1);
-// const element2 = createAndAddNavbarElement(section2);
-// const element3 = createAndAddNavbarElement(section3);
 navbarList.style.cssText = "color: black; display: flex; justify-content: space-around; height: 40px; padding: 10px 0";
 const navbarElementsList = navbarList.querySelectorAll("li");
 for(i = 0; i < navbarElementsList.length; i++){
@@ -86,16 +72,6 @@ function scrollToSection(){
 }
 scrollToSection();
 
-
-// element1.addEventListener('click', function(){
-//     section1.scrollIntoView({behavior: "smooth"});
-// })
-// element2.addEventListener('click', function(){
-//     section2.scrollIntoView({behavior: "smooth"});
-// })
-// element3.addEventListener('click', function(){
-//     section3.scrollIntoView({behavior: "smooth"});
-// })
 /**
  * End Main Functions
  * Begin Events
@@ -108,7 +84,15 @@ scrollToSection();
 
 // Set sections as active
 
-// navbarElementsList.forEach(function(element){
-//     element.classList.remove("your-active-class");
-// });
+function setAsActive(){
+    sections.forEach((element, index) =>{
+        navbarElementsList[index].addEventListener('click', function(){
+            sections.forEach((element, index) =>{
+               element.classList.remove("your-active-class");});
+        element.classList.add("your-active-class");})
+        });
+};
+
+setAsActive();
+
 
