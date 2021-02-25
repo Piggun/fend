065e84ced7657b645f7b1b912538e7c64d25a4d2
Riccardo Foti <riccardofoti97@gmail.com>
2021-02-25 18:34:12 +0000

    Cleaned up code

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -18,13 +18,13 @@
  * 
 */
 
-const pageHeader = document.querySelector(".page__header");
-const navbarList = document.getElementById("navbar__list");
-const sections = document.querySelectorAll("section");
-const section1 = document.getElementById("section1");
-const section2 = document.getElementById("section2");
-const section3 = document.getElementById("section3");
-const pageFold = window.pageYOffset + 800;
+const pageHeader = document.querySelector('.page__header');
+const navbarList = document.getElementById('navbar__list');
+const sections = document.querySelectorAll('section');
+const section1 = document.getElementById('section1');
+const section2 = document.getElementById('section2');
+const section3 = document.getElementById('section3');
+const pageFold = document.body.scrollHeight / 3
 const sectionTitles = document.querySelectorAll('h2');
 
 
@@ -52,6 +52,7 @@ function addScrollToTopElWhenBelowPageFold(){
  * 
 */
 
+
 // build the nav
 function createNavbar(){
     for(let item of sections){
@@ -68,9 +69,9 @@ function setAsActive(){
     sections.forEach((element, index) =>{
         navbarElementsList[index].addEventListener('click', function(){     // add EventListener to every element in navbar
             sections.forEach((element, index) =>{
-               element.classList.remove("your-active-class");});        // remove class 'your-active-class' from every section
-            if (!element.classList.contains("isCollapsed")){        // if section is not collapsed
-                element.classList.add("your-active-class");}})       // add class 'your-active-class' to corresponding section
+               element.classList.remove('your-active-class');});        // remove class 'your-active-class' from every section
+            if (!element.classList.contains('isCollapsed')){        // if section is not collapsed
+                element.classList.add('your-active-class');}})       // add class 'your-active-class' to corresponding section
         });
 }
 
@@ -80,11 +81,11 @@ function setNavbarElActive(){
     sections.forEach((element, index) =>{       // loop through every section
         let box = element.getBoundingClientRect();
         if(box.top <= 150 && box.bottom >= 150){        // if section is in viewport
-            navbarElementsList[index].classList.add("active");      // add class 'active' to element in navbar
+            navbarElementsList[index].classList.add('active');      // add class 'active' to element in navbar
         }
         else{
-            navbarElementsList[index].classList.remove("active");       // remove class 'active' from element in navbar
-        }})
+            navbarElementsList[index].classList.remove('active');       // remove class 'active' from element in navbar
+        }});
 }
 
 
@@ -92,7 +93,7 @@ function setNavbarElActive(){
 function scrollToSection(){
     for(let i = 0; i < navbarElementsList.length; i++){
         navbarElementsList[i].addEventListener('click', function(){
-            sections[i].scrollIntoView({behavior: "smooth"});
+            sections[i].scrollIntoView({behavior: 'smooth'});
         });
     };
 }
@@ -104,28 +105,28 @@ function hideNavbar(){
     window.addEventListener('scroll',function(){
         let currentScrollPosition = window.pageYOffset;     // Assigns current scroll position
         if(startingScrollPos > currentScrollPosition +10 ){      // If scrolling up  / "+10" adds a bit of deadzone
-            pageHeader.style.top = "0";     // Keeps navbar visible
+            pageHeader.style.top = '0';     // Keeps navbar visible
         }
         else if(startingScrollPos < currentScrollPosition - 10){     // If scrolling down  / "-10" adds a bit of deadzone
-            pageHeader.style.top = "-60px";       // Hides navbar
+            pageHeader.style.top = '-60px';       // Hides navbar
         }
         startingScrollPos =  currentScrollPosition;     // Updates startingScrollPos to the current scroll position
-    })
+    });
 }
 
 
 // Creates an element that scrolls the viewport to the top of the page when clicked
 function createScrollToTopEl(){
-    scrollToTop = document.createElement("li");     // This variable has global scope because it has NOT been declared
-    scrollToTop.innerText = ("^");
-    scrollToTop.id = "scrollToTop";
-    scrollToTop.className = "menu__link";
+    scrollToTop = document.createElement('li');     // This variable has global scope because it has NOT been declared
+    scrollToTop.innerText = ('^');
+    scrollToTop.id = 'scrollToTop';
+    scrollToTop.className = 'menu__link';
     scrollToTop.addEventListener('click', function(){
         window.scrollTo({       // Scroll to top of the page
             top: 0,
-            behavior: "smooth"
+            behavior: 'smooth'
         });
-    })
+    });
 }
 
 
@@ -133,18 +134,19 @@ function createScrollToTopEl(){
 function collapseSection(){
     sections.forEach((section, index) =>{
         sectionTitles[index].addEventListener('click', function(){
-            section.classList.toggle("isCollapsed")
-            this.nextElementSibling.classList.toggle("collapsed");      // collapse/open first paragraph when section's title is clicked
-            if (section.classList.contains("your-active-class")){       // if section is active
-                section.classList.remove("your-active-class");      // make it inactive
+            section.classList.toggle('isCollapsed')
+            this.nextElementSibling.classList.toggle('collapsed');      // collapse/open first paragraph when section's title is clicked
+            if (section.classList.contains('your-active-class')){       // if section is active
+                section.classList.remove('your-active-class');      // make it inactive
             }
-            else if (!section.classList.contains("your-active-class") && !section.classList.contains("isCollapsed")){       // if section is NOT active and NOT collapsed
+            else if (!section.classList.contains('your-active-class') && !section.classList.contains("isCollapsed")){       // if section is NOT active and NOT collapsed
                 sections.forEach((section) => {
-                    section.classList.remove("your-active-class");      // make every section inactive
+                    section.classList.remove('your-active-class');      // make every section inactive
                 });
-            section.classList.add("your-active-class");}        // make section active
-            this.nextElementSibling.nextElementSibling.classList.toggle("collapsed");       // collapse/open first paragraph when section's title is clicked
-            })
+                section.classList.add('your-active-class');        // make section active
+            };
+            this.nextElementSibling.nextElementSibling.classList.toggle('collapsed');       // collapse/open first paragraph when section's title is clicked
+            });
         }
     );
 }
@@ -159,10 +161,10 @@ function collapseSection(){
 
 // Build menu 
 createNavbar();
-navbarList.style.cssText = "color: black; display: flex; justify-content: space-around; height: 40px; padding: 10px 0";
-const navbarElementsList = navbarList.querySelectorAll("li");
+navbarList.style.cssText = 'color: black; display: flex; justify-content: space-around; height: 40px; padding: 10px 0';
+const navbarElementsList = navbarList.querySelectorAll('li');
 for(i = 0; i < navbarElementsList.length; i++){
-    navbarElementsList[i].style.cssText= "padding: 10px 5px";     // Adds padding to every element inside navbarList
+    navbarElementsList[i].style.cssText= 'padding: 10px 5px';     // Adds padding to every element inside navbarList
 };
 
 
